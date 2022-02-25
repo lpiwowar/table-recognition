8dae4ccdec9a480d63340a6afe20ce285b184d66
Lukas Piwowarski <lukas.piwowarski@gmail.com>
2022-02-25 17:50:33 +0100

    Add --dataset-prep argument

--- table_recognition.py
+++ table_recognition.py
@@ -2,20 +2,47 @@ import argparse
 
 from config_parser import Config
 
+
+def data_preparation(config):
+
+    pass
+
+
+def check_arguments(arg):
+    """
+    A function that checks whether valid combination of
+    arguments is used.
+
+    :type arg:  ArgumentParser
+    :param arg: Parsed arguments that should be checked.
+    :return:     True when valid combination of arguments is used
+                 False otherwise
+    """
+    return not (not arg.train ^ arg.infer) ^ arg.data_preparation
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Table recognition")
     parser.add_argument("--train",
-                        help="Train model for table recognition (--config_file required)",
+                        help="Train model for table recognition (--config-file required)",
                         action="store_true")
     parser.add_argument("--infer",
-                        help="Use trained model for table recognition (--config_file required)",
+                        help="Use trained model for table recognition (--config-file required)",
                         action="store_true")
-    parser.add_argument("--config_file",
+    parser.add_argument("--data-preparation",
+                        help="Prepare dataset for training (--config-file required)",
+                        action="store_true")
+    parser.add_argument("--config-file",
                         help="Path to configuration file",
                         default="./table_recognition_config.ini")
     args = parser.parse_args()
 
-    if not args.train ^ args.infer:
-        raise Exception("ERROR: Either --train or --infer must be specified.")
+    if not check_arguments(args):
+        raise Exception("ERROR: Either --train, --infer or --data-preparation must be specified.")
 
     config = Config(args.config_file)
+
+    if args.data_preparation:
+        data_preparation(config)
+    # for file in os.listdir(config.train_input_data_dir):
+    #   print(file)
