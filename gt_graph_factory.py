44f23e7ff2d679740b730b749dfa7ac36092b689
Lukáš Piwowarski (personal) <lpiwowar-personal@localhost.localdomain>
2022-03-01 18:59:10 +0100

    Color nodes

--- gt_graph_factory.py
+++ gt_graph_factory.py
@@ -1,4 +1,5 @@
 import os
+from operator import itemgetter
 from xml.etree import ElementTree
 
 import cv2
@@ -31,6 +32,9 @@ class GTGraphCreator(object):
         self.rtree_id_to_text_line = {}              # Directory that maps rtree ID to OCRTextLine instance
         self.edges = set()                           # List of edges between the textlines [[1,2], [2,3], [4,5], ...]
 
+        self.table_cells = []                        # List of table cells from the ground truth
+        self.d_rtree_id_to_text_line = {}            # Directory that maps rtree ID to DatasetGTNode
+
     def create_k_nearest_neighbors_graphs(self, k_param=4):
         """
         Create graph representation of XML CTDAR ground truth data.
@@ -55,18 +59,51 @@ class GTGraphCreator(object):
             self.rtree_id_to_text_line[text_line.rtree_id] = text_line
             rtree_id += 1
 
-            x_coords = [x for x, _ in text_line.polygon_points]
-            y_coords = [y for _, y in text_line.polygon_points]
-            max_x, min_x = max(x_coords), min(x_coords)
-            max_y, min_y = max(y_coords), min(y_coords)
-            text_line.bbox_points = (min_x, min_y, max_x, max_y)
             idx.insert(text_line.rtree_id, text_line.bbox_points)
+
+        # Parse XML from the dataset
+        cells_xml = ElementTree.parse(self.dataset_gt_path).findall("./table/cell")
+        self.table_cells += [DatasetGTNode(cell_xml) for cell_xml in cells_xml]
+
+        # Create Rtree index
+        idx_cells = index.Index()
+        rtree_id = 0
+        for table_cell in self.table_cells:
+            table_cell.rtree_id = rtree_id
+            self.d_rtree_id_to_text_line[table_cell.rtree_id] = table_cell
+            rtree_id += 1
+
+            idx_cells.insert(table_cell.rtree_id, table_cell.bbox_points)
 
         # Create graph representation
         for text_line in self.text_lines:
+            # Create edges and define its types
             k_neighbors = list(idx.nearest(text_line.bbox_points, k_param))
             self.edges = self.edges.union({(text_line.rtree_id, neighbor) for neighbor in k_neighbors})
             self.edges = self.edges.union({(neighbor, text_line.rtree_id) for neighbor in k_neighbors})
+
+            # Define type of node in graph
+            cells_intersection = list(idx_cells.intersection(text_line.bbox_points))
+            if cells_intersection:
+                dataset_gt_nodes = self.get_dataset_gt_nodes_by_rtree_idxs(cells_intersection)
+                cells_intersection_types = [cell.type for cell in dataset_gt_nodes]
+                cell_type = max(cells_intersection_types, key=cells_intersection_types.count)
+                text_line.type = cell_type
+
+    def get_dataset_gt_nodes_by_rtree_idxs(self, rtree_idxs):
+        """
+        A function that takes as an input a list of rtree_idxs and
+        returns a list of corresponding dataset_gt_objects
+
+        :type rtree_idxs:  List of integers
+        :param rtree_idxs: List of rtree indeces
+        :return:           List of DatasetGTNode objects
+        """
+        dataset_gt_nodes = itemgetter(*rtree_idxs)(self.d_rtree_id_to_text_line)
+        if type(dataset_gt_nodes) is tuple:
+            return list(dataset_gt_nodes)
+        else:
+            return [dataset_gt_nodes]
 
     def visualize_graph(self, visualize_dir="./dataset_preparation/visualization"):
         """
@@ -76,25 +113,107 @@ class GTGraphCreator(object):
         :type visualize_dir:  String
         :param visualize_dir: Directory that should store the visualizations
         """
+        colors = {
+            "node": {
+                "header": (0, 255, 0),
+                "data": (255, 0, 0),
+                "data_empty": (230, 230, 250),
+                "header_empty": (255, 248, 220),
+                None: (0, 0, 0)
+            },
+            "edge": {
+
+            }
+        }
 
         img = cv2.imread(self.dataset_image_path)
 
         for text_line in self.text_lines:
-            center_x, center_y = text_line.bbox_center()
-            cv2.circle(img, (center_x, center_y),  radius=10, color=(0, 0, 255), thickness=-1)
+            # Draw polygon that defines region for a given text line
+            left, bottom, right, top = text_line.bbox_points
+            cv2.rectangle(img, (left, bottom), (right, top), color=(0, 0, 255), thickness=2)
 
+            # Draw edges for a given node
             neighbors = [edge_dst for edge_src, edge_dst in self.edges if edge_src == text_line.rtree_id]
-
             for neighbor in neighbors:
                 src_pt = self.rtree_id_to_text_line[text_line.rtree_id].bbox_center()
                 dst_pt = self.rtree_id_to_text_line[neighbor].bbox_center()
                 cv2.line(img, src_pt, dst_pt, color=(0, 0, 0), thickness=3)
 
+            # Draw graphs node (circle in the middle of the text line)
+            center_x, center_y = text_line.bbox_center()
+            cv2.circle(img, (center_x, center_y), radius=10, color=colors["node"][text_line.type], thickness=-1)
+
         image_name = self.dataset_image_path.split("/")[-1]
         cv2.imwrite(os.path.join(visualize_dir, "GRAPH_" + image_name), img)
 
 
-class OCRTextLine(object):
+class Edge(object):
+    def __init__(self):
+        self.node_left = None
+        self.node_right = None
+        self.type = None
+
+
+class PolygonNode(object):
+    """
+    This class represents a region in an image that is defined
+    by a polygon.
+
+    The region is defined by XML tag. And the properties of the
+    region are used to define a node in a graph that is used
+    to train the GNN.
+
+    :type text_line_xml:  XMLElement
+    :param text_line_xml: XML element representing coordinates of a given text line
+    """
+    def __init__(self, text_line_xml):
+        """
+        A constructor of PolygonNode class
+
+        :type text_line_xml:  XMLElement
+        :param text_line_xml: XML element representing coordinates of a given text line
+        """
+
+        self.text_line_xml = text_line_xml
+        self.polygon_points = None
+        self.bbox_points = None
+        self.rtree_id = None
+
+    def parse(self):
+        """Parser of the information from the :param text_line_xml:"""
+        raise Exception(f"parse() not implemented!")
+
+    def calculate_bbox_points(self):
+        """
+        Calculate a bounding box of a given polygon specified
+        in the :param polygon_points: variable.
+        """
+
+        x_coords = [x for x, _ in self.polygon_points]
+        y_coords = [y for _, y in self.polygon_points]
+        max_x, min_x = max(x_coords), min(x_coords)
+        max_y, min_y = max(y_coords), min(y_coords)
+        self.bbox_points = (min_x, min_y, max_x, max_y)
+
+    def bbox_center(self):
+        """
+        Returns 2D point that is in the middle of the bounding box
+        defined by polygon points
+
+        :return: tuple that contains coordinates of 2D point that is in
+                 the middle of the bounding box
+        """
+        left, bottom, right, top = self.bbox_points
+        width = abs(right - left)
+        height = abs(top - bottom)
+        center_x = int(left + width / 2)
+        center_y = int(top - height / 2)
+
+        return center_x, center_y
+
+
+class OCRTextLine(PolygonNode):
     """
     Class representing a given text line.
 
@@ -107,12 +226,68 @@ class OCRTextLine(object):
     """
 
     def __init__(self, text_line_xml):
-        """
+        super().__init__(text_line_xml)
+
+        self.uuid = None  # UUID that represents the text line in XML
+        self.type = None
+        self.parse()
+        self.calculate_bbox_points()
+
+    def parse(self):
+        """Parser that parser information from the :param text_line_xml:"""
+        ns = {"xmlns": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15",
+              "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+              "xsi:schemaLocation": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15/pagecontent.xsd"}
+
+        self.uuid = self.text_line_xml.attrib["id"]
+
+        coords_xml = self.text_line_xml.find("./xmlns:Coords", ns)
+        self.polygon_points = coords_string_to_tuple_list(coords_xml.attrib["points"])
+
+
+class DatasetGTNode(PolygonNode):
+    """
+    Class representing a polygon that defines a table cell in the dataset.
+
+    The class takes as an input XML element representing given table cell
+    The XML element is parsed by the class and attributes of the element
+    are stored inside the object.
+
+    :type text_line_xml:  XMLElement
+    :param text_line_xml: XML element representing coordinates of a given text line
+    """
+
+    def __init__(self, text_line_xml):
+        super().__init__(text_line_xml)
+
+        self.start_row = None
+        self.end_row = None
+        self.start_col = None
+        self.end_col = None
+        self.type = None
+
+        self.parse()
+        self.calculate_bbox_points()
+
+    def parse(self):
+        """Parser that parser information from the :param text_line_xml:"""
+        self.start_row = int(self.text_line_xml.attrib["start-row"])
+        self.end_row = int(self.text_line_xml.attrib["end-row"])
+        self.start_col = int(self.text_line_xml.attrib["start-col"])
+        self.end_col = int(self.text_line_xml.attrib["end-col"])
+        self.type = str(self.text_line_xml.attrib["type"])
+
+        coords_xml = self.text_line_xml.find("./Coords")
+        self.polygon_points = coords_string_to_tuple_list(coords_xml.attrib["points"])
+
+"""
+    def __init__(self, text_line_xml):
+        ""
         A constructor for OCRNode class
 
         :param text_line_xml: XML element representing a given text line
 
-        """
+        ""
         self.text_line_xml = text_line_xml  # XML representation of TextLine
         self.uuid = None  # UUID that represents the text line in XML
         self.rtree_id = None  # ID that represents the polygon in rtree db
@@ -133,23 +308,36 @@ class OCRTextLine(object):
 
         coords_xml = self.text_line_xml.find("./xmlns:Coords", ns)
         self.polygon_points = coords_string_to_tuple_list(coords_xml.attrib["points"])
+        self.calculate_bbox_points()
+        
+    def calculate_bbox_points(self):
+        ""
+        Calculate a bounding box of a given polygon specified
+        in the :param polygon_points: variable.
+        ""
+
+        x_coords = [x for x, _ in self.polygon_points]
+        y_coords = [y for _, y in self.polygon_points]
+        max_x, min_x = max(x_coords), min(x_coords)
+        max_y, min_y = max(y_coords), min(y_coords)
+        self.bbox_points = (min_x, min_y, max_x, max_y)
 
     def bbox_center(self):
-        """
+        ""
         Returns 2D point that is in the middle of the bounding box
         defined by polygon points
 
         :return: tuple that contains coordinates of 2D point that is in
                  the middle of the bounding box
-        """
+        ""
         left, bottom, right, top = self.bbox_points
         width = abs(right - left)
         height = abs(top - bottom)
         center_x = int(left + width / 2)
-        center_y = int(top + height / 2)
+        center_y = int(top - height / 2)
 
         return center_x, center_y
-
+"""
 
 """
 class Table(object):
