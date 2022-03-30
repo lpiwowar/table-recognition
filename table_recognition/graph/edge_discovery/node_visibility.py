66ade3dfb851502b691ff050badcf68140670d6d
Lukas Piwowarski <lukas.piwowarski@gmail.com>
2022-03-30 18:33:19 +0200

    Finish rotation of 180 degrees

--- table_recognition/graph/edge_discovery/node_visibility.py
+++ table_recognition/graph/edge_discovery/node_visibility.py
@@ -22,36 +22,48 @@ class NodeVisibility(object):
         boxes_image = self.render_boxes_image()
         import time
         # for node in self.graph.nodes:
-        for x in range(0, 90):
-            line_array = self.get_line_array((self.img_h // 2, self.img_w // 2), x)
+        for x in range(0, 181):
+            line_array = self.get_line_array_in_180(x)
             cv2.imshow("test", line_array)
-            time.sleep(1)
             cv2.waitKey(0)
             # print(line_array)
 
         pass
 
-    def get_line_array(self, point, angle_deg):
+    def get_line_array_in_180(self, angle_deg):
+        assert 0 <= angle_deg <= 180, "ERROR: get_line_array_in_180() accepts deg value in range <0,180>"
+
+        if 0 <= angle_deg <= 90:
+            return self.get_line_array_in_90(angle_deg)
+        else:
+            line_array = self.get_line_array_in_90(180 - angle_deg)
+            return np.flip(line_array, 0)
+
+    def get_line_array_in_90(self, angle_deg):
+        assert 0 <= angle_deg <= 90, "ERROR: get_line_array_in_90() accepts deg value in range <0,90>"
+
         angle_rad = (np.pi / 180) * angle_deg
 
         center_x = self.img_w // 2
         center_y = self.img_h // 2
 
-        # Angle in (0, 45)
+        # Angle in (0, 45>
         if angle_deg < 45:
             x_diff = np.floor(np.tan(angle_rad) * center_x)
             start_point = (self.img_h - 1, center_x - x_diff)
             end_point = (0, center_x + x_diff)
+
+        # Angle in <45, 90>
         elif 45 <= angle_deg <= 90:
             y_diff = np.tan((np.pi/2) - angle_rad) * center_y
             start_point = (center_y + y_diff, 0)
             end_point = (center_y - y_diff, self.img_w - 1)
-        
-        line_coords = line_nd(start_point, end_point)
-        img = np.zeros((self.img_h, self.img_w), dtype=np.uint8)
-        img[line_coords] = 255
 
-        return img
+        line_coords = line_nd(start_point, end_point)
+        line_array = np.zeros((self.img_h, self.img_w), dtype=np.uint8)
+        line_array[line_coords] = 255
+
+        return line_array
 
     def render_boxes_image(self):
         """Generate 2D-array in which each pixels says id of node it represents"""
