7aefd8e95582b712aac80ddea020313530fbc85b
Lukas Piwowarski <lukas.piwowarski@gmail.com>
2022-04-05 11:37:30 +0200

    Log batch size

--- table_recognition/train.py
+++ table_recognition/train.py
@@ -78,7 +78,8 @@ class Trainer(object):
             name=datetime.datetime.now().strftime("%Y-%m-%d-%H:%M"),
             mode=self.conf.wandb_mode,
             config=hyperparameters,
-            learning_rate=self.conf.learning_rate
+            learning_rate=self.conf.learning_rate,
+            batch_size=self.conf.batch_size
         )
 
         with wandb.init(**wandb_params):
