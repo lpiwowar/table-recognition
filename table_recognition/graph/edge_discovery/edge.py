5335849ce2bec77967ab59ea47b9c8344f816bf2
Lukas Piwowarski <lukas.piwowarski@gmail.com>
2022-04-19 17:39:55 +0200

    Add help functions for Node

--- table_recognition/graph/edge_discovery/edge.py
+++ table_recognition/graph/edge_discovery/edge.py
@@ -1,5 +1,6 @@
 import uuid
 
+
 class Edge(object):
     def __init__(self, node1, node2):
         self.uuid = uuid.uuid1()
@@ -11,6 +12,9 @@ class Edge(object):
         self.input_feature_vector = None
         self.output_feature_vector = None
 
+        # Indicates whether the edge influences the finale shape of the graph
+        self.gridify_locked = False
+
     def __eq__(self, other):
         return self.node1.id == other.node1.id and \
                self.node2.id == other.node2.id
@@ -19,7 +23,20 @@ class Edge(object):
         return hash(self.__repr__())
 
     def __repr__(self):
-        return f"<Edge: node1={self.node1} node2={self.node2}>"
+        return f"({self.node1}, {self.node2})"
 
     def is_reflexive(self):
         return self.node1 == self.node2
+
+    def connects(self, node1_id, node2_id):
+        if node1_id == self.node1.id and node2_id == self.node2.id or \
+           node1_id == self.node2.id and node2_id == self.node1.id:
+            return True
+        else:
+            return False
+
+    def get_connected_node(self, node_id):
+        if self.node1.id == node_id:
+            return self.node2
+        else:
+            return self.node1
